ea7443b19cb9a2c792d6edf74ff93e14d72a6e7b
“GABYNEVE” <gabineveoliveira06@gmail.com>
2025-08-27 16:34:04 -0300

    Tabela Sql e Melhor formatação de inputs

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Db_Usuarios_FlyWay;
+USE Db_Usuarios_FlyWay;
+
+CREATE TABLE Usuarios
+(
+	Usuario_ID int identity (1,1) PRIMARY KEY,
+	Usuario_Email VARCHAR(350) NOT NULL,
+	Usuario_Nome VARCHAR(40) NOT NULL,
+	Usuario_Telefone VARCHAR(20) NOT NULL,
+	Usuario_Senha VARCHAR(40 )NOT NULL
+);
+
+ALTER TABLE Usuarios
+ADD CONSTRAINT CHK_Usuario_Telefone
+CHECK (
+    Usuario_Telefone LIKE '(__) ____-____'   -- Telefone fixo
+    OR
+    Usuario_Telefone LIKE '(__) _____-____'  -- Celular
+);
+
